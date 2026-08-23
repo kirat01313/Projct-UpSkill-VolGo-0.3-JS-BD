75a2b1a558cde35b1533a9de132cec6b93cef409
Tarik Chaia <tguaranho@gmail.com>
2026-08-23 22:54:01 +0100

    Esse commit termina de implementar o menuPostos.js, adicionando as funções que faltavam de atualizar posto (alterarPosto1) e remover posto (excluirPosto1). Também foi criada uma função de validação para evitar a inserção de um código de posto errado na hora de usar as funções de alterar e remover postos.

--- 1-algoritmia/src/1-cli/cli-tarik/menuPostos.js
+++ 1-algoritmia/src/1-cli/cli-tarik/menuPostos.js
@@ -1,9 +1,37 @@
 import readlineSync from "readline-sync";
 import { CONCELHOS_VALIDOS, CONECTORES_VALIDOS, ESTADOS_POSTO_VALIDOS } from "../../utils/constantes.js";
 import { lerNumeroPositivo, lerOpcaoValida, lerTextoObrigatorio } from "../../utils/validacao.js";
-import { listarPostos } from "../../2-repositories/postoRepository.js";
+import { listarPostos, inserirPosto1, alterarPosto1, procurarPosto1, excluirPosto1 } from "../../2-repositories/postoRepository.js";
 
 const prompt = readlineSync.question;
+
+//----Funções auxiliares do menu----
+function inserirNovoPosto() {
+  const codigoPosto = lerTextoObrigatorio("Digite o código do novo Posto: ");
+  const concelhoPosto = lerOpcaoValida("Digite o concelho do novo Posto: ", CONCELHOS_VALIDOS);
+  const potenciaPosto = lerNumeroPositivo("Digite a potência do novo Posto: ");
+  const conectorPosto = lerOpcaoValida("Digite o tipo de conector do novo Posto: ", CONECTORES_VALIDOS);
+  const estadoPosto = lerOpcaoValida("Digite o estado atual do novo Posto: ", ESTADOS_POSTO_VALIDOS);
+
+  const novoPosto = {
+    codigo: codigoPosto, concelho: concelhoPosto,
+    potenciaKw: potenciaPosto, tipoConector: conectorPosto, estado: estadoPosto
+  }; return novoPosto
+}
+
+function lercodigoPosto(acao) {
+  let codigo = lerTextoObrigatorio(`Digite o Código do posto a ser ${acao} (ou 0 para cancelar): `).trim().toUpperCase();
+  while (codigo !== "0" && procurarPosto1(codigo) === null) {
+    console.log("Esse código não existe.");
+    codigo = lerTextoObrigatorio(`Código (ou 0 para cancelar): `).trim().toUpperCase();
+  }
+  if (codigo === "0") {
+    return null; // Retorna null se o usuário escolher cancelar a operação
+  }
+  return codigo;
+}
+
+//----Menu de Postos----
 
 export function menuPostos() {
   console.log('1. Listar Postos cadastrados');
@@ -15,37 +43,47 @@ export function menuPostos() {
   const opcao = prompt('Escolha uma opção: ');
 
   switch (opcao) {
-    case '1':
+    case '1': {
       console.log(`Os postos cadastrados atualmente no nosso sistema são: `);
       console.log(listarPostos());
       break;
-    case '2':
-      function inserirNovoPosto() {
-        const codigoPosto = lerTextoObrigatorio("Digite o código do novo Posto: ");
-        const concelhoPosto = lerOpcaoValida("Digite o concelho do novo Posto: ", CONCELHOS_VALIDOS);
-        const potenciaPosto = lerNumeroPositivo("Digite a potência do novo Posto: ");
-        const conectorPosto = lerOpcaoValida("Digite o tipo de conector do novo Posto: ", CONECTORES_VALIDOS);
-        const estadoPosto = lerOpcaoValida("Digite o estado atual do novo Posto: ", ESTADOS_POSTO_VALIDOS);
+    }
+    case '2': {
+      const novoPosto1 = inserirNovoPosto(); //Função que capta dados do novo posto e retorna um objeto com esses dados
+      const resultado = inserirPosto1(novoPosto1); //função que insere o novo posto no JSON e retorna null se o posto já existir lá
 
-        const novoPosto = {
-          codigo: codigoPosto, concelho: concelhoPosto,
-          potenciaKw: potenciaPosto, tipoConector: conectorPosto, estado: estadoPosto
-        }; return novoPosto
+      if (resultado === null) {
+        console.log("Já existe um posto com esse código.\n");
+      } else {
+        console.log("Posto inserido com sucesso.\n");
       }
-      inserirNovoPosto();
-
-
-      //Chamar a função para inserir posto na lista
-      //Passar uma mensgem confirmando que foi inserido um posto na lista
-      //Perguntar se quer inserir mais um ou voltar ao menu principal
-
-      console.log('TODO: submenu de Inserir Postos ainda não implementado.\n');
       break;
-    case '3':
-      console.log('TODO: submenu de Atualizar Posto ainda não implementado.\n');
+    }
+
+    case '3': {
+
+      const codigoPosto = lercodigoPosto("atualizado"); //Guarda o código do posto a ser atualizado
+      if (codigoPosto === null) {
+        break;
+      }
+      const alterarDadosPosto = alterarPosto1(codigoPosto, inserirNovoPosto()); //Função que capta dados do novo posto e retorna um objeto com esses dados
+      if (alterarDadosPosto === null) {
+        console.log("Não existe um posto com esse código.\n");
+      } else {
+        console.log("Posto atualizado com sucesso.\n");
+      }
       break;
+    }
     case '4':
-      console.log('TODO: submenu de Remover Posto ainda não implementado.\n');
+      const excluirPosto = excluirPosto1(lercodigoPosto("excluído")); //Função que exclui o posto do JSON e retorna true se excluiu, ou false se não encontrou o posto
+      if (excluirPosto === null) {
+        break;
+      }
+      if (excluirPosto === false) {
+        console.log("Não existe um posto com esse código ou ele possui carregamentos associados.\n"); //Mensagem de erro caso o posto não exista ou tenha carregamentos associados
+      } else {
+        console.log("Posto excluído com sucesso.\n");
+      }
       break;
     case '0':
       console.log('Até já!');
@@ -54,7 +92,7 @@ export function menuPostos() {
       console.log('Opção inválida.\n');
   }
 
-  menuPostos();
+  menuPostos(); //função recursiva para repetir o menu até que o usuário escolha sair
 }
 
 menuPostos();
--- 1-algoritmia/src/2-repositories/postoRepository.js
+++ 1-algoritmia/src/2-repositories/postoRepository.js
@@ -1,25 +1,76 @@
 import path from 'node:path'; //biblioteca do proprio node para definir caminhos de ficheiros
 import { readFileSync, writeFileSync, existsSync } from 'node:fs'; //ler ficheiro, escrever ficheiro, verificar se existe ficheiro
 
-const caminhoPostos = path.join(import.meta.dirname, '../../data/postos.json'); //define o endereço do ficheiro e o nome do 
-                                                                                //ficheiro em uma constante
+const caminhoPostos = path.join(import.meta.dirname, '../../data/postos.json'); //define o endereço do ficheiro e o nome do ficheiro em uma constante
+
 
 export function listarPostos() {
-  if (!existsSync(caminhoPostos))                        //verifica se existe ficheiro naquele endereço/nome que indicamos 
-    return [];                                           // se não existir retorna um array vazio e já sai da função, a linha a seguir nem chega a rodar
-  const conteudo = readFileSync(caminhoPostos, 'utf-8'); //Caso exista já o ficheiro ele lê o conteudo 'utf-8' converte os dados para string
-  if (conteudo.trim() === "")                            //se existir um ficheiro mas estiver vazio, faz o mesmo que antes, 
-    return [];                                           //retorna um array vazio e já sai da função, a linha a seguir nem chega a rodar 
-  return JSON.parse(conteudo);                           //Caso tenha algo, reconverte o texto para um array e retorna esse array
+    if (!existsSync(caminhoPostos))                        //verifica se existe ficheiro naquele endereço/nome que indicamos 
+        return [];                                           // se não existir retorna um array vazio e já sai da função, a linha a seguir nem chega a rodar
+    const conteudo = readFileSync(caminhoPostos, 'utf-8'); //Caso exista já o ficheiro ele lê o conteudo 'utf-8' converte os dados para string
+    if (conteudo.trim() === "")                            //se existir um ficheiro mas estiver vazio, faz o mesmo que antes, 
+        return [];                                           //retorna um array vazio e já sai da função, a linha a seguir nem chega a rodar 
+    return JSON.parse(conteudo);                           //Caso tenha algo, reconverte o texto para um array e retorna esse array
 }
 
+export function procurarPosto1(codigo) { //Função que procura um posto pelo código, devolvendo o objeto do posto se encontrado, ou null se não encontrado
+    const postos = listarPostos();
+    for (let i = 0; i < postos.length; i++) {
+        if (postos[i].codigo === codigo) return postos[i];
+    }
+    return null;
+}
 
+export function inserirPosto1(posto) {
+    const postos = listarPostos(); //chama a função listarPostos() para pegar o array de postos já existentes
+    postos.push(posto); //acrescenta o novo posto ao array de postos
+    gravarPostos(postos); //chama a função gravarPostos() para gravar o array atualizado no ficheiro
+    return posto; //devolve o posto inserido
+}
 
+export function alterarPosto1(codigo, dadosNovos) {
+    const postos = listarPostos(); //chama a função listarPostos() para pegar o array de postos já existentes
+    let postoIndex = -1;
+    for (let i = 0; i < postos.length; i++) { //percorre o array de postos para encontrar o posto com o código fornecido
+        if (postos[i].codigo === codigo) { //se encontrar, guarda o índice do posto no array
+            postoIndex = i; //guarda o índice do posto no array
+            break;
+        }
+    }
+    if (postoIndex === -1) {//se não encontrar, devolve null (essa parte parece duplicada com a parte do menuPostos.js)
+        return null;
+    } else { //se encontrar, atualiza os campos do posto com os dados novos fornecidos
+        postos[postoIndex].concelho = dadosNovos.concelho;
+        postos[postoIndex].potenciaKw = dadosNovos.potenciaKw;
+        postos[postoIndex].tipoConector = dadosNovos.tipoConector;
+        postos[postoIndex].estado = dadosNovos.estado;
+        gravarPostos(postos); //chama a função gravarPostos() para gravar o array atualizado no ficheiro
+        return postos[postoIndex]; //devolve o posto atualizado
+    }
+}
 
+export function excluirPosto1(codigo) { //Função que exclui um posto pelo código, devolvendo true se excluiu, ou false se não encontrou o posto
+    // TODO (quando o Fred tiver o carregamentoRepository):
+    //   integridade referencial — não remover se houver carregamentos neste posto
+    //   const carregamentos = listarCarregamentos();
+    //   for (let i = 0; i < carregamentos.length; i++) {
+    //     if (carregamentos[i].posto === codigo) return false;
+    //   }
 
+    const postos = listarPostos(); //chama a função listarPostos() para pegar o array de postos já existentes
+    const restantes = []; //cria um array vazio para guardar os postos que não serão excluídos
+    for (let i = 0; i < postos.length; i++) {
+        if (postos[i].codigo !== codigo) {     //se o código do posto atual não for igual ao código fornecido, adiciona o posto ao array de restantes
+            restantes.push(postos[i]);
+        }
+    }
+    if (restantes.length === postos.length) return false;   //se o array de restantes tiver o mesmo tamanho que o array original, significa que não encontrou o posto com o código fornecido, então devolve false
+    gravarPostos(restantes); //chama a função gravarPostos() para gravar o array de restantes no ficheiro, sobrescrevendo o array original
+    return true; //devolve true, indicando que o posto foi excluído com sucesso
+}
 
-function gravarPostos(postos) {                                           //sem export. só usada aqui dentro pois reescreve nos dados o que foi alterado
-  writeFileSync(caminhoPostos, JSON.stringify(postos, null, 2), 'utf-8'); //Função utilizada por outras funções
+function gravarPostos(postos) { //Função que grava/regrava o array de postos no ficheiro
+    writeFileSync(caminhoPostos, JSON.stringify(postos, null, 2), 'utf-8');
 }
 
 
